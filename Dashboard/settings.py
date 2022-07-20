39fade70c5a34dca4e892312588495109d4881a5
FeFedez <f_er_0205@hotmail.com>
2022-07-20 13:27:51 -0500

    adding static and media
    this tes is for add static and media

--- Dashboard/settings.py
+++ Dashboard/settings.py
@@ -109,15 +109,22 @@ TIME_ZONE = 'UTC'
 
 USE_I18N = True
 
+USE_L10N = True
+
 USE_TZ = True
 
 
 # Static files (CSS, JavaScript, Images)
 # https://docs.djangoproject.com/en/4.0/howto/static-files/
 
-STATIC_URL = 'static/'
-
 # Default primary key field type
 # https://docs.djangoproject.com/en/4.0/ref/settings/#default-auto-field
 
 DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
+
+# default static files settings for PythonAnywhere.
+# see https://help.pythonanywhere.com/pages/DjangoStaticFiles for more info
+MEDIA_ROOT = '/home/FeFedez/Dashboard/media'
+MEDIA_URL = '/media/'
+STATIC_ROOT = '/home/FeFedez/Dashboard/static'
+STATIC_URL = '/static/'
